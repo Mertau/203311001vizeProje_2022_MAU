a9cde82bd7e08f90d5417134538a5c755afe8142
Mertau <mertahmetusta@msn.com>
2022-04-03 17:40:04 +0300

    Deneme commit

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:stylish/components/coustom_bottom_nav_bar.dart';
+import 'package:stylish/constants.dart';
+import 'package:stylish/enums.dart';
+import 'package:stylish/screens/login/login_screen.dart';
+import 'package:stylish/screens/profile/profile_screen.dart';
+import 'components/categories.dart';
+import 'components/new_arrival_products.dart';
+import 'components/popular_products.dart';
+import 'components/search_form.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CustomBottomNavBar(selectedMenu: MenuState.profile),
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset("assets/icons/Location.svg"),
+            const SizedBox(width: defaultPadding / 2),
+            Text(
+              "Konya",
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset("assets/icons/Notification.svg"),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
+        padding: const EdgeInsets.all(defaultPadding),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Keşfet",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4!
+                  .copyWith(fontWeight: FontWeight.w500, color: Colors.black),
+            ),
+            const Text(
+              "Tarzına Göre",
+              style: TextStyle(fontSize: 18),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: defaultPadding),
+              child: SearchForm(),
+            ),
+            const Categories(),
+            const NewArrivalProducts(),
+            const PopularProducts(),
+          ],
+        ),
+      ),
+    );
+  }
+}
